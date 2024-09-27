94ab9ca70e288a9afeb33a1d038480a7298346d1
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-26 21:03:03 -0700

    added part-3

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //import "./styles.css";
 
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
 //*********Part 1*********/
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
@@ -11,6 +18,14 @@ const navEl = document.getElementById('top-menu');
 navEl.style.height = '100%';
 navEl.style.backgroundColor = 'var(--top-menu-bg)';
 navEl.classList.add("flex-around");
+
+//*********Part 3*********/
+for (const ele of menuLinks) {
+    const aEl = document.createElement('a');
+    aEl.setAttribute('href', ele.href);
+    aEl.textContent = ele.text;
+    navEl.appendChild(aEl);
+}
 
 
 
