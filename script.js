fa142270f038e03ecf467fce0705aeb0606a37f7
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-26 20:10:25 -0700

    added part-1

---
+++ script.js
@@ -0,0 +1,8 @@
+//import "./styles.css";
+
+//*********Part 1*********/
+//console.log(`*******Part-1******`);
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+mainEl.classList.add("flex-ctr");
