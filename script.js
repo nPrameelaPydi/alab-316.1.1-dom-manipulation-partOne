1db3b94150a39ed52f0b2320f42518b3bcc4e8cc
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-26 20:17:20 -0700

    added part-2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //import "./styles.css";
 
 //*********Part 1*********/
-//console.log(`*******Part-1******`);
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add("flex-ctr");
+
+//*********Part 2*********/
+const navEl = document.getElementById('top-menu');
+navEl.style.height = '100%';
+navEl.style.backgroundColor = 'var(--top-menu-bg)';
+navEl.classList.add("flex-around");
+
+
+
+
